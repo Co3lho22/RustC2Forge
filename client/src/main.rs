fbc5914d1bfe05b46d0e4e8c2043ff4415ebd0f5
Co3lho22 <co3lho22@gmail.com>
2024-03-04 22:27:17 +0000

    make the client connect presistent

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,37 @@
-use std::io::{Read, Write, Result};
+use std::io::{self, Read, Write, Result};
 use std::net::TcpStream;
 
 fn main() -> Result<()> {
     let mut stream = TcpStream::connect("192.168.1.70:7878")?;
+    println!("Connect to the server.");
 
-    let message = "This is the windows 11 machine";
-    stream.write_all(message.as_bytes())?;
+    loop {
+        let mut input = String::new();
+        println!("Enter a message to send to the server:");
+        io::stdin().read_line(&mut input)?;
+        if input.trim() == "exit" {
+            println!("Exiting.");
+            break;
+        }
 
-    let mut buffer = vec![0; 1024];
-    let nbyte = stream.read(&mut buffer)?;
+        stream.write_all(input.as_bytes())?;
 
-    let response_str = String::from_utf8_lossy(&buffer[..nbyte]);
-    println!("Response: {}", response_str);
+        let mut buffer = vec![0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(0) => {
+                println!("Server closed the connection.");
+                break;
+            },
+            Ok(n) => {
+                let response_str = String::from_utf8_lossy(&buffer[..n]);
+                println!("Response from server: {}", response_str);
+            },
+            Err(e) => {
+                println!("An error occured: {}", e);
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
