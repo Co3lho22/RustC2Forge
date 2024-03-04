79d659e0a051bdf1b2c8d80861bb0f277ed504ae
co3lho22 <co3lho22@gmail.com>
2024-03-04 16:46:23 +0000

    basic tcp server with echo funcionality

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            let _ = stream.write_all(&mut buffer[0..size]);
+            true
+        },
+        Err(_) => {
+            println!(
+                "An error occured, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
+            stream.shutdown(std::net::Shutdown::Both).unwrap();
+            false
+        }
+    } {}
 }
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    println!("Server listening on port 7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(|| handle_client(stream));
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
+    drop(listener);
+}
+
+
